5834925762c8aa3ba4091421be91a76b922a223c
Brent Westbrook <brentrwestbrook@gmail.com>
2023-09-12 09:01:00 -0400

    sort bins

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 setup(
     scripts=[
+        "bin/create_fb_inputs.py",
+        "bin/create_msm.py",
         "bin/download_dataset.py",
         "bin/select_parameters.py",
-        "bin/create_msm.py",
-        "bin/create_fb_inputs.py",
     ],
 )
